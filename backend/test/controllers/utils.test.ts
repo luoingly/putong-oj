6ddb00dadd6b277099fc1bd0545ae4cc891a2339
luoingly <signature@luoingly.top>
2026-03-29 18:48:50 +0800

    test: update file upload tests

--- backend/test/controllers/utils.test.ts
+++ backend/test/controllers/utils.test.ts
@@ -1,8 +1,11 @@
+import { Buffer } from 'node:buffer'
 import fs from 'node:fs'
 import { resolve } from 'node:path'
 import test from 'ava'
 import supertest from 'supertest'
 import app from '../../src/app'
+import Files from '../../src/models/Files'
+import User from '../../src/models/User'
 import { encryptData } from '../../src/services/crypto'
 import { deploy } from '../../src/utils/constants'
 
@@ -55,8 +58,10 @@ test('Admin could submit file', async (t) => {
     .attach('image', filepath)
 
   t.is(submit.status, 200)
+  t.true(submit.body.success)
 
-  const { url } = submit.body
+  const { url } = submit.body.data
+  t.is(typeof url, 'string')
   const uploaded = url.match(/\/uploads\/(.*)/)[1]
 
   const uploadedContent = fs.readFileSync(
@@ -64,4 +69,49 @@ test('Admin could submit file', async (t) => {
     'utf8',
   )
   t.is(uploadedContent, content)
+
+  const record = await Files.findOne({ storageKey: uploaded }).lean()
+  t.truthy(record)
+  t.is(record?.sizeBytes, Buffer.byteLength(content, 'utf8'))
+})
+
+test('Normal user upload should be denied when storage quota is 0', async (t) => {
+  const login = await request
+    .post('/api/account/login')
+    .send({
+      username: 'primaryuser',
+      password: await encryptData('testtest'),
+    })
+  t.is(login.status, 200)
+
+  const submit = await request
+    .post('/api/upload')
+    .attach('image', filepath)
+
+  t.is(submit.status, 200)
+  t.false(submit.body.success)
+  t.is(submit.body.code, 403)
+})
+
+test('Normal user upload should succeed after storage quota is set', async (t) => {
+  const user = await User.findOne({ uid: 'primaryuser' })
+  t.truthy(user)
+  user!.storageQuota = 2 * 1024 * 1024
+  await user!.save()
+
+  const login = await request
+    .post('/api/account/login')
+    .send({
+      username: 'primaryuser',
+      password: await encryptData('testtest'),
+    })
+  t.is(login.status, 200)
+
+  const submit = await request
+    .post('/api/upload')
+    .attach('image', filepath)
+
+  t.is(submit.status, 200)
+  t.true(submit.body.success)
+  t.is(typeof submit.body.data.url, 'string')
 })
